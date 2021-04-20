aad7ca51d7477078f0f2a45010142ca7adc75f1b
Anish Yadav <yadavanish893@gmail.com>
2021-04-20 16:38:24 -0700

    source code

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div class="row">
+                            <div class="col-md-12">
+                                <div class="copyright">
+                                    <p>CVMS 2021. A product by Anish Yadav.</p>
+                                </div>
+                            </div>
+                        </div>
